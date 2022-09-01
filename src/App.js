2c2764c471ddc4ba531200549e5775647b1a4a7d
reaper <reaper@reapers-MacBook-Air.local>
2022-09-01 22:31:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "reaper Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
@@ -26,28 +25,8 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <h2>reaper</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].title}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].title}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].title}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].title}
-        date={expenses[3].date}
-      ></ExpenseItem>
+    <div>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+function Expenses(props) {
+  const expenses = props.expenses;
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].title}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].title}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].title}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].title}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;

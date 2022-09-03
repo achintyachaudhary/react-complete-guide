dfea7398004b97b433cf312f0340c60e383af335
reaper <reaper@reapers-MacBook-Air.local>
2022-09-03 21:46:04 +0530

    /

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,66 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titlechangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountchangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const datechangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const formData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(formData);
+    setEnteredAmount(NaN);
+    setEnteredDate("");
+    setEnteredTitle("");
+  };
   return (
     <div className="expense_form">
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text"></input>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titlechangeHandler}
+            ></input>
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="1" step="1"></input>
+            <input
+              type="number"
+              value={enteredAmount}
+              min="1"
+              step="1"
+              onChange={amountchangeHandler}
+            ></input>
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2023-01-01"></input>
+            <input
+              type="date"
+              value={enteredDate}
+              min="2019/01/01"
+              max="2023/01/01"
+              onChange={datechangeHandler}
+            ></input>
           </div>
           <div className="new-expense__control">
             <button type="submit">Add Expense</button>
